67d4245c45f39f76f667fdcfebd20338fa9b1273
marilynias <98028585+marilynias@users.noreply.github.com>
2022-01-27 11:58:36 +0100

    setup on 2nd mashine, procedual generated buttons

--- Game Selector/Assets/scripts/Import_games.cs
+++ Game Selector/Assets/scripts/Import_games.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Import_games : MonoBehaviour
 {
@@ -17,16 +19,33 @@ public class Import_games : MonoBehaviour
     void Start()
     {
         bttn_prefab.transform.SetParent(parent.transform);
+        //string bttn_prefab.filepath = "test";
 
-        string gamepath = Path.Combine(@"D:","Projects");
-        UnityEngine.Debug.Log(gamepath);
-        UnityEngine.Debug.Log(Directory.GetDirectories(gamepath));
+
+        string gamepath = Path.Combine(@"P:", "Python");
+
+        int posx = 0;
+        int posy = 20;
+
+        //UnityEngine.Debug.Log(gamepath);
+        //UnityEngine.Debug.Log(Directory.GetDirectories(gamepath));
 
 
         foreach (string folder in Directory.GetDirectories(gamepath)){
-            UnityEngine.Debug.Log(Path.Combine(gamepath, folder));
-            string[] filePaths = Directory.GetFiles(Path.Combine(gamepath, folder));
-            UnityEngine.Debug.Log(filePaths);
+            //UnityEngine.Debug.Log(Path.Combine(gamepath, folder));
+            foreach(string file in Directory.GetFiles(Path.Combine(gamepath, folder)))
+            {
+                posy -= 6;
+                string path = Path.Combine(gamepath, folder, file);
+                UnityEngine.Debug.Log(file);
+                GameObject bttn = Instantiate(bttn_prefab, parent.transform);
+                bttn.GetComponentInChildren<Text>().text = file;
+                Button btn = bttn.GetComponent<Button>();
+                btn.onClick.AddListener(delegate { start_Process(file); });
+                bttn.transform.position = new Vector3(0, posy, posx);
+            }
+            //string[] filePaths = Directory.GetFiles(Path.Combine(gamepath, folder));
+            
         }
 
 
@@ -39,8 +58,9 @@ public class Import_games : MonoBehaviour
         
     }
 
-    void start_Process()
+    void start_Process(string path)
     {
+        UnityEngine.Debug.Log("executing " + path);
         string command = "notepad";
         ProcessStartInfo proc_start_info = new ProcessStartInfo("cmd", "/c " + command);
         proc_start_info.CreateNoWindow = true;
--- Game Selector/Assets/scripts/start_Game.cs
+++ Game Selector/Assets/scripts/start_Game.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public string path;
     // Start is called before the first frame update
     void Start()
     {
