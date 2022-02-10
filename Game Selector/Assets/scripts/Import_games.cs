fa8c0c5f09bdea20d935d7bf54e0b3ee7d5aa7a2
marilynias <98028585+marilynias@users.noreply.github.com>
2022-02-10 12:20:28 +0100

    Update Import_games.cs

--- Game Selector/Assets/scripts/Import_games.cs
+++ Game Selector/Assets/scripts/Import_games.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Import_games : MonoBehaviour
 {
@@ -36,17 +37,31 @@ public class Import_games : MonoBehaviour
             foreach(string file in Directory.GetFiles(Path.Combine(gamepath, folder)))
             {
                 posy -= 6;
-                string path = Path.Combine(gamepath, folder, file);
-                UnityEngine.Debug.Log(file);
+                //string path = Path.Combine(gamepath, folder, file);
+                string[] directories = file.Split(Path.DirectorySeparatorChar);
+                //var last = directories.Last();
+                //UnityEngine.Debug.Log(directories[2]);
+                //string filename = file[1];
                 GameObject bttn = Instantiate(bttn_prefab, parent.transform);
-                bttn.GetComponentInChildren<Text>().text = file;
+                bttn.GetComponentInChildren<Text>().text = directories[2];
+                //bttn.AddComponent(Button);
                 Button btn = bttn.GetComponent<Button>();
-                btn.onClick.AddListener(delegate { start_Process(file); });
+                //btn.onClick.AddListener(() => Start_Process(file));
+                //btn.onClick.AddListener(() => Test("aa"));
+
+                var targetInfo = UnityEvent.GetValidMethodInfo(this, nameof(Test), new Type[0]);
+                UnityAction methodDelegate = Delegate.CreateDelegate(typeof(UnityAction), this, targetInfo) as UnityAction;
+                UnityEventTools.AddPersistentListener(BigExplosionEvent, methodDelegate);
+
+
+                //UnityEngine.Debug.Log(btn);
                 bttn.transform.position = new Vector3(0, posy, posx);
             }
             //string[] filePaths = Directory.GetFiles(Path.Combine(gamepath, folder));
             
         }
+
+        
 
 
 
@@ -58,7 +73,7 @@ public class Import_games : MonoBehaviour
         
     }
 
-    void start_Process(string path)
+    public UnityEngine.Events.UnityAction Start_Process(string path)
     {
         UnityEngine.Debug.Log("executing " + path);
         string command = "notepad";
@@ -67,5 +82,14 @@ public class Import_games : MonoBehaviour
         Process proc = new System.Diagnostics.Process();
         proc.StartInfo = proc_start_info;
         proc.Start();
+        return null;
     }
+    UnityAction Test(string a)
+    {
+        UnityEngine.Debug.Log("bttn pressed" + a);
+        return null;
+    }
+
+
+
 }
