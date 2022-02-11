af4a40927274efcc4aa1e42ad7903d8237d119ac
marilynias <98028585+marilynias@users.noreply.github.com>
2022-02-11 12:15:24 +0100

    Buttons work

--- Game Selector/Assets/scripts/Import_games.cs
+++ Game Selector/Assets/scripts/Import_games.cs
@@ -5,7 +5,9 @@ using System.IO;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEditor;
 
 public class Import_games : MonoBehaviour
 {
@@ -13,6 +15,8 @@ public class Import_games : MonoBehaviour
     public Canvas parent;
 
     public GameObject bttn_prefab;
+
+    public string key;
 
 
 
@@ -24,40 +28,57 @@ public class Import_games : MonoBehaviour
 
 
         string gamepath = Path.Combine(@"P:", "Python");
+        string[] a_ext = { ".py", ".txt" };
+        List<string> allowed_ext = new List<string>(a_ext);
+        
 
-        int posx = 0;
-        int posy = 20;
+        int posx = -100;
+        int posy = 10;
 
         //UnityEngine.Debug.Log(gamepath);
         //UnityEngine.Debug.Log(Directory.GetDirectories(gamepath));
-
+        //Button btn = bttn_prefab.GetComponent<Button>();
+        //btn.onClick.AddListener(Test);
 
         foreach (string folder in Directory.GetDirectories(gamepath)){
             //UnityEngine.Debug.Log(Path.Combine(gamepath, folder));
             foreach(string file in Directory.GetFiles(Path.Combine(gamepath, folder)))
             {
+                string ext = Path.GetExtension(file);
+                if (!allowed_ext.Contains(ext))
+                {
+                    UnityEngine.Debug.Log("not valid ext: " + ext);
+                    continue;
+                }
                 posy -= 6;
+                if (posy < -100)
+                {
+                    UnityEngine.Debug.Log("new row");
+                    posy = 10;
+                    posx += 100;
+                }
                 //string path = Path.Combine(gamepath, folder, file);
                 string[] directories = file.Split(Path.DirectorySeparatorChar);
                 //var last = directories.Last();
                 //UnityEngine.Debug.Log(directories[2]);
                 //string filename = file[1];
                 GameObject bttn = Instantiate(bttn_prefab, parent.transform);
-                bttn.GetComponentInChildren<Text>().text = directories[2];
+                bttn.GetComponentInChildren<TextMeshProUGUI>().text = Path.GetFileNameWithoutExtension(directories[2]);
                 //bttn.AddComponent(Button);
                 Button btn = bttn.GetComponent<Button>();
-                //btn.onClick.AddListener(() => Start_Process(file));
-                //btn.onClick.AddListener(() => Test("aa"));
+                btn.onClick.AddListener(() => Start_Process(file));
+                //btn.onClick.AddListener(Test);
 
-                var targetInfo = UnityEvent.GetValidMethodInfo(this, nameof(Test), new Type[0]);
-                UnityAction methodDelegate = Delegate.CreateDelegate(typeof(UnityAction), this, targetInfo) as UnityAction;
-                UnityEventTools.AddPersistentListener(BigExplosionEvent, methodDelegate);
+                //var targetinfo = UnityEvent.GetValidMethodInfo(myScriptInstance, "OnButtonClick", new Type[] { typeof(GameObject) });
+                //UnityAction methodDelegate = Delegate.CreateDelegate(typeof(UnityAction), this, targetInfo) as UnityAction;
+                //UnityEditor.Events.UnityEventTools.AddPersistentListener(btn.onClick, methodDelegate);
 
 
                 //UnityEngine.Debug.Log(btn);
                 bttn.transform.position = new Vector3(0, posy, posx);
             }
             //string[] filePaths = Directory.GetFiles(Path.Combine(gamepath, folder));
+            //UnityEngine.Debug.Log(posy);
             
         }
 
@@ -70,24 +91,38 @@ public class Import_games : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //UnityEngine.Debug.Log(EventSystem.current);
+        //if (Input.GetKeyDown(key))
+        //{
+        //    EventSystem.current.SetSelectedGameObject(this.gameObject);
+        //}
     }
 
     public UnityEngine.Events.UnityAction Start_Process(string path)
     {
         UnityEngine.Debug.Log("executing " + path);
-        string command = "notepad";
-        ProcessStartInfo proc_start_info = new ProcessStartInfo("cmd", "/c " + command);
-        proc_start_info.CreateNoWindow = true;
-        Process proc = new System.Diagnostics.Process();
-        proc.StartInfo = proc_start_info;
-        proc.Start();
+        //var ext = Path.GetExtension(path);
+        //string command = "";
+        //switch (ext)
+        //{
+        //    case ".txt":
+        //        command = "notepad";
+        //        break;
+
+
+        //}
+
+        //ProcessStartInfo proc_start_info = new ProcessStartInfo("cmd", "/c " + command);
+        //proc_start_info.CreateNoWindow = true;
+        //Process proc = new Process();
+        //proc.StartInfo = proc_start_info;
+        Process.Start(@path);
         return null;
     }
-    UnityAction Test(string a)
+    void Test()
     {
-        UnityEngine.Debug.Log("bttn pressed" + a);
-        return null;
+        UnityEngine.Debug.Log("bttn pressed");
+        //return null;
     }
 
 
